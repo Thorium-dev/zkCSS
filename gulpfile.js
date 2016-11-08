2dfe1795457e2e76a841301dfc45cd018f4db09f
Thorium-dev <fkdiomande@gmail.fr>
2016-11-08 09:32:58 +0100

    Ajout du fichier gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
-/**
- * Created by Franck on 07/11/2016.
- */
+const gulp = require('gulp');
+const configs = require('./gulpConfig').configs;
+const requires = {};
+
+var task, pipes, pipe, stream;
+
+// Récupération des modules de chaque tâche pour les placer dans le tableau requires
+for(task in configs){
+    pipes = configs[task].pipes;
+   for(pipe in pipes){
+       requires[pipe] = require('gulp-'+pipe);
+   }
+}
+
+// Définition de chaque tache depuis le fichier de configuration
+for(task in configs){
+    pipes = configs[task].pipes;
+    gulp.task(task, function () {
+        stream = gulp.src(configs[task].src);
+        for(pipe in pipes){
+            stream = stream.pipe(requires[pipe](pipes[pipe]));
+        }
+        stream.pipe(gulp.dest(configs[task].dest));
+    });
+}
